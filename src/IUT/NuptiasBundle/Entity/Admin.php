4406f20dda1e93e6ed634398553eae444b514d25
Mohameth <mohameth38@outlook.fr>
2016-11-24 07:13:21 +0100

    Fosuser

--- src/IUT/NuptiasBundle/Entity/Admin.php
+++ src/IUT/NuptiasBundle/Entity/Admin.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="admin")
  * @ORM\Entity(repositoryClass="IUT\NuptiasBundle\Repository\AdminRepository")
  */
-class Admin
+class Admin extends User
 {
     /**
      * @var int
@@ -19,21 +19,7 @@ class Admin
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nom", type="string", length=35, unique=true)
-     */
-    private $nom;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="mdp", type="string", length=35, unique=true)
-     */
-    private $mdp;
+    protected $id;
 
 
     /**
@@ -45,53 +31,4 @@ class Admin
     {
         return $this->id;
     }
-
-    /**
-     * Set nom
-     *
-     * @param string $nom
-     *
-     * @return Admin
-     */
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    /**
-     * Get nom
-     *
-     * @return string
-     */
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    /**
-     * Set mdp
-     *
-     * @param string $mdp
-     *
-     * @return Admin
-     */
-    public function setMdp($mdp)
-    {
-        $this->mdp = $mdp;
-
-        return $this;
-    }
-
-    /**
-     * Get mdp
-     *
-     * @return string
-     */
-    public function getMdp()
-    {
-        return $this->mdp;
-    }
 }
-
